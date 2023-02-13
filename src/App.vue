690853af097b8f979e7883976edb8222f1ea3be7
LorenzoNicco <lorenzoniccolai91@gmail.com>
2023-02-13 12:17:59 +0100

    Add scaffholding base

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script>
+import AppHeader from "./components/AppHeader.vue";
+import AppMain from "./components/AppMain.vue";
+import AppFooter from "./components/AppFooter.vue";
+
+export default {
+  data() {
+    return {
+      
+    }
+  },
+  components: {
+    AppHeader,
+    AppMain,
+    AppFooter
+  }
+}
+</script>
+
+<template>
+  <AppHeader />
+
+  <AppMain />
+
+  <AppFooter />
+</template>
+
+<style scoped>
+
+</style>
