2d9b71cc68def857db696a55b987be8e93efde84
LorenzoNicco <lorenzoniccolai91@gmail.com>
2023-02-15 11:38:10 +0100

    Add carosello testimonials

--- src/store.js
+++ src/store.js
@@ -1,5 +1,24 @@
 import { reactive } from "vue"
 
 export const store = reactive ({
-
+    testimonials: [
+        {
+            text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo facere autem enim qui. Sunt fuga, eveniet nostrum deleniti ea quidem, nisi molestiae architecto similique unde voluptatibus impedit, ducimus quae molestias voluptas ut repellendus. Voluptatum eum sunt repellendus reprehenderit aperiam ex?",
+            pic: "/cinema/img/assets/01.jpg",
+            name: "JOHN DOE",
+            job: "Designer"
+        },
+        {
+            text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo facere autem enim qui.",
+            pic: "/cinema/img/assets/02.jpg",
+            name: "MARY JAMES",
+            job: "Photographer"
+        },
+        {
+            text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia veniam nulla, adipisci, laudantium sapiente illo magni quo, commodi quaerat pariatur voluptatum alias libero exercitationem eligendi!",
+            pic: "/cinema/img/assets/03.jpg",
+            name: "CHRIS COLEMAN",
+            job: "Videomaker"
+        }
+    ]
 })
